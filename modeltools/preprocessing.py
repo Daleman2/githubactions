274624d132433223f3d9de7737bc35d7d57b5a2d
Daleman2 <don.aleman@gmail.com>
2022-10-03 19:18:03 +0000

    new test for preprocessing

---
+++ modeltools/preprocessing.py
@@ -0,0 +1,13 @@
+"""
+
+Este módulo contiene helpers para el preprocesamiento de datos
+
+"""
+
+import numpy as np
+
+
+
+def get_numerical_features(df):
+
+    return list(df.select_dtypes(include=[np.number]).columns)
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from modeltools.preprocessing import get_numerical_features
+
+def test_get_numerical_features_simple():
+    """ En este test vamos a probar que logra disnguir entre cadenas de texto 
+    y numeros enteros"""
+
+    
+
+    df = pd.DataFrame({
+
+        "numerica": [5],
+        "categorica": ["rojo"]
+
+    })
+
+    #Assert es "como un IF" pero que falle si la condicion es falsa. Esto es ideal para los test.
+
+    assert get_numerical_features(df) == ["numerica"]
+
+
+def test_get_numerical_features_empty():
+
+    """Este test comprueba que se devuelve una lista vacía si no hay ninguna variable num."""
+
+    df = pd.DataFrame({
+
+         "categorica": ["rojo"]
+
+    })
+
+    #Assert es "como un IF" pero que falle si la condicion es falsa. Esto es ideal para los test.
+
+    assert get_numerical_features(df) == []
